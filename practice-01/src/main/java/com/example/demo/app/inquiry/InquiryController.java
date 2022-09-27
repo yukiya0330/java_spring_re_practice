7d2ac16a4b3cf768346ea0d6959562323fa1d072
yukiya <hosiyky33077@gmail.com>
2022-09-27 21:55:47 +0900

    commit

--- practice-01/src/main/java/com/example/demo/app/inquiry/InquiryController.java
+++ practice-01/src/main/java/com/example/demo/app/inquiry/InquiryController.java
@@ -5,33 +5,49 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/inquiry")
 public class InquiryController {
 
 	@GetMapping("/form")
-	public String form(InquiryForm inquiryForm, Model model) {
+	public String form(InquiryForm inquiryForm,
+			Model model,
+//			フラッシュスコープの値をhtmlでレンダリングする処理
+			@ModelAttribute("complete") String complete) {
 		model.addAttribute("title", "Inquiry Form");
 		return "inquiry/form";
 	}
 
-//	値をPostした時に、失敗したらFormに戻す、成功したらconfirmページにいく
 	@PostMapping("/confirm")
-//	@ValidatedでバリデーションがInquiryFormにかかる
 	public String confirm(@Validated InquiryForm inquiryForm,
-//			バリデーション後の結果
 			BindingResult result,
 			Model model) {
-//		バリデーションに引っかかった場合の処理
 		if(result.hasErrors()) {
 			model.addAttribute("title", "Inquiry Form");
 			return "inquiry/form";
 		}
 		model.addAttribute("title", "Confirm Page");
 		return "inquiry/confirm";
+	}
+
+//	confirm完了処理　失敗したら、Formに戻される、成功した保存されFormに戻される
+	@PostMapping("/complete")
+	public String complete(@Validated InquiryForm inquiryForm,
+			BindingResult result,
+			Model model,
+//			リダイレクト処理
+			RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			model.addAttribute("title", "Inquiry Form");
+			return "inquiry/form";
+		}
+		redirectAttributes.addAttribute("complete", "Registerd");
+		return "redirect:/inquiry/form";
 	}
 
 }
